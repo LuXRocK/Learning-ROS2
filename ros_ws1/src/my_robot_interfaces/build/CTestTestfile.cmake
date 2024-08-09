570c1a8b66894ed9f062ec3309ba48324821dec3
LuXRocK <samuelujazda@gmail.com>
2024-08-09 14:02:10 +0200

    started working on the next section

---
+++ ros_ws1/src/my_robot_interfaces/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/samuel/Learning_ROS/ros_ws1/src/my_robot_interfaces
+# Build directory: /home/samuel/Learning_ROS/ros_ws1/src/my_robot_interfaces/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
